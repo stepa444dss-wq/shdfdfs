1731f43ab87095a2e6030dcde6cb0f1ad0722f29
stepa444dss-wq <stepa444dss@gmail.com>
2026-07-12 20:26:46 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,26 +1,29 @@
-
 if CLIENT then
-    local frame = vgui.Create("DFrame")
-    frame:SetSize(400, 250)         
-    frame:Center()               
-    frame:SetTitle("test") 
-    frame:MakePopup()      
+    concommand.Add("jopa", function()
+        local frame = vgui.Create("DFrame")
+        frame:SetSize(400, 250)         
+        frame:Center()               
+        frame:SetTitle("test") 
+        frame:MakePopup()      
 
-    local label = vgui.Create("DLabel", frame)
-    label:SetPos(20, 50)         
-    label:SetText("test")
-    label:SizeToContents()          
+        local label = vgui.Create("DLabel", frame)
+        label:SetPos(20, 50)         
+        label:SetText("test")
+        label:SizeToContents()          
 
-    local button = vgui.Create("DButton", frame)
-    button:SetSize(150, 40)
-    button:SetPos(20, 90)
-    button:SetText("test")
-    
-    button.DoClick = function()
-        LocalPlayer():ChatPrint("test")
-    end
-    local textInput = vgui.Create("DTextEntry", frame)
-    textInput:SetSize(200, 25)
-    textInput:SetPos(20, 150)
-    textInput:SetPlaceholderText("test")
+        local button = vgui.Create("DButton", frame)
+        button:SetSize(150, 40)
+        button:SetPos(20, 90)
+        button:SetText("test")
+        
+        button.DoClick = function()
+            LocalPlayer():ChatPrint("test")
+        end
+
+        local textInput = vgui.Create("DTextEntry", frame)
+        textInput:SetSize(200, 25)
+        textInput:SetPos(20, 150)
+        textInput:SetPlaceholderText("test")
+        
+    end)
 end
